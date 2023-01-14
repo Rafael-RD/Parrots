edad436b1e1dc48283538da9b4f728e2afdc3b21
Rafael <rafaelreinertd@gmail.com>
2023-01-14 03:30:14 -0300

    Bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-let qtdpares=0;
+let qtdcartas=0;
 let tempImgFrente='';
 let jogadas=0;
+let idInterval;
+let timer=0;
 const imgFrente=['<img src="img/bobrossparrot.gif" alt="1">',
 '<img src="img/explodyparrot.gif" alt="2">',
 '<img src="img/fiestaparrot.gif" alt="3">',
@@ -12,18 +14,17 @@ tempImgFrente=randomizer(imgFrente);
 const contCartas=document.querySelector('.container');
 function criarJogo(){
     jogadas=0;
-    if(arguments[0]!==undefined)    qtdpares=arguments[0];
+    if(arguments[0]!==undefined)    qtdcartas=arguments[0];
     else{
-        qtdpares=0;
-        while(qtdpares<2||qtdpares>6){
-            qtdpares = prompt("Com quantos pares você deseja jogar? (de 2 a 6 pares) -1 para sair");
-            if(qtdpares<0)  return;
+        qtdcartas=0;
+        while(qtdcartas<4 || qtdcartas>14 || qtdcartas%2!=0){
+            qtdcartas = prompt("Com quantas cartas você deseja jogar? (de 4 a 14 e par)");
         }
     }
     contCartas.innerHTML='';
     let tempContCartas=[];
 
-    for(let i=qtdpares;i>0;i--){
+    for(let i=qtdcartas/2;i>0;i--){
         tempContCartas.push(`
         <div class="card" onclick="clickCarta(this)">
             <div class="front-face face virada">
@@ -47,6 +48,11 @@ function criarJogo(){
     }
     tempContCartas=randomizer(tempContCartas);
     contCartas.innerHTML=tempContCartas.join('');
+    timer=0;
+    idInterval = setInterval(() => {
+        timer++;
+        document.querySelector('.contador').innerText=timer;
+    }, 1000);
 }
 
 //Fisher–Yates Shuffle
@@ -64,11 +70,19 @@ function randomizer(array){
 
 function checar(){
     console.log(contCartas.querySelectorAll('.acertou').length);
-    if(contCartas.querySelectorAll('.acertou').length>=qtdpares*2){
+    if(contCartas.querySelectorAll('.acertou').length>=qtdcartas){
+        clearInterval(idInterval);
         setTimeout(() => {
-            alert(`Você ganhou em ${jogadas} jogadas!`);
-            setTimeout(criarJogo,1000);
-            }, 200);
+            alert(`Você ganhou em ${jogadas} jogadas! A duração do jogo foi de ${timer} segundos!`);
+            setTimeout(() => {
+                let resp=0;
+                while(resp!=='não' && resp!=='sim'){
+                    resp=prompt('Reiniciar a partida?(sim ou não)');
+                    if(resp==='sim') criarJogo();
+                    else if(resp==='não') return;
+                }
+            }, 1000);
+        }, 200);
     }
 }
 
